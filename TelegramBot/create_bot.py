6ad153c82aeba9d44bb56eea8a5302092c888152
c0lbarator <youknowmaps@gmail.com>
2025-01-20 23:27:41 +0300

    fixed redis using

--- TelegramBot/create_bot.py
+++ TelegramBot/create_bot.py
@@ -1,10 +1,13 @@
 import logging
+
+from aiogram.fsm.storage.base import DefaultKeyBuilder
+
 from TelegramBot.config import config
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.redis import RedisStorage
 
 bot = Bot(token=config.token.get_secret_value())
-dp = Dispatcher(storage=RedisStorage.from_url(config.redis_storage.get_secret_value()))
+dp = Dispatcher(storage=RedisStorage.from_url(config.redis_storage.get_secret_value(), key_builder=DefaultKeyBuilder(with_destiny=True)))
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
